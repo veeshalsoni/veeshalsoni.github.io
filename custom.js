74fdfc3b350b201c727569343923770d8b3e7eb7
Vishal Soni <vishal3soni@gmail.com>
2018-05-08 11:32:59 +0530

    fixed css, added social icons

--- custom.js
+++ custom.js
@@ -1,10 +1,12 @@
-$(window).scroll(function() {
-  $(".slideanim").each(function(){
-    var pos = $(this).offset().top;
+$(document).ready(function(){
+	$(window).scroll(function() {
+	  $(".slideanim").each(function(){
+	    var pos = $(this).offset().top;
 
-    var winTop = $(window).scrollTop();
-    if (pos < winTop + 600) {
-      $(this).addClass("slide");
-    }
-  });
-}); 
+	    var winTop = $(window).scrollTop();
+	    if (pos < winTop + 600) {
+	      $(this).addClass("slide");
+	    }
+	  });
+	}); 
+});
